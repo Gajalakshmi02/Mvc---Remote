649e0cda3e4979c945208218e74fa75b06cfe483
Gajalakshmi02 <164175599+Gajalakshmi02@users.noreply.github.com>
2024-03-21 13:22:58 +0530

    Add files via upload

---
+++ Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using MVCRemote.Common;
+
+namespace MVCRemote.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+    }
+    public class UserMetadata
+    {
+        [RemoteClientServer("IsUserNameAvailable1", "Home", ErrorMessage = "UserName already in use")]
+        public string UserName { get; set; }
+    }
+}
